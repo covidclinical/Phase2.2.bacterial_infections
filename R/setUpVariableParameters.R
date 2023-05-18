8fa5f8c86d3c5c971d7e06e1eea2c0fe811cdfd0
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2023-05-18 20:03:18 +0000

    script to keep all the internal parameters

---
+++ R/setUpVariableParameters.R
@@ -0,0 +1,16 @@
+## minimum days hospitalized
+min_hosp_days <- 1 
+
+## time period for the analysis
+## (possible values: days, weeks, months )
+time_period <-"months"
+
+## cut-off dates
+pandemic_start_date <- as.Date("2020-03-01")
+start_date_plots <- as.Date("2018-12-01")
+end_date_plots <- as.Date("2023-01-03")
+
+## Colorblind palette
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+
