9241d77e28fe506fd931c2a1c101e5eb004e2d8b
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2023-05-19 20:06:20 +0000

    function to get the ICD counts

--- R/qc_summary.R
+++ R/qc_summary.R
@@ -1,12 +1,24 @@
-qc_summary <- function(obs_df, obfuscation_threshold, dir.output){
+qc_summary <- function(input_df, obfuscation_threshold, dir.output){
   
   print("Starting QC summary")
   
-  if(during_misc_hosp){
-    complete_df <- complete_df %>% filter(n_hospitalisation == 1)
-  }
+  ### check that the ICD codes follow the regular expression [A-Z][0-9][0-9AB]\.?[0-9A-TV-Z]{0,4}
+  codesToReview <- input_df %>% dplyr::filter(concept_type == "DIAG-ICD10",
+                                             ! grepl( "[A-Z][0-9][0-9AB]\\.?[0-9A-TV-Z]{0,4}", concept_code))
+  print(codesToReview)
   
-  outcome_summary <- complete_df %>%
+  ### summary of the ICD codes for QC
+  diag_sum <- input_df %>%
+    filter( concept_type == 'DIAG-ICD10') %>%
+    group_by( concept_code ) %>%
+    summarise( n_patients = n_distinct( patient_num ) ) %>%
+    mutate( n_patients = ifelse( n_patients > obfuscation_threshold | isFALSE( obfuscation_threshold), n_patients, 0.5)) %>%
+    arrange( desc(n_patients))
+  
+  save( diag_sum, file=paste0( dir.output, "ICDdiagnosisCodes.RData"))
+  ###
+  
+  outcome_summary <- input_df %>%
     group_by(patient_num) %>%
     summarise(ever_in_hospital = max(in_hospital),
               ever_severe = max(severe),
@@ -17,108 +29,9 @@ qc_summary <- function(obs_df, obfuscation_threshold, dir.output){
   print(paste0("Total number of patients ever in icu: ", sum(outcome_summary$ever_in_icu)))
   print(paste0("Total number of patients ever dead: ", sum(outcome_summary$ever_dead)))
   
-  total_n <- length(unique(complete_df$patient_num))
-  
-  labs_of_interest <- read.delim(system.file(paste0("extdata", .Platform$file.sep,
-                                                    "laboratoryCharacteristics.txt"), package = "FourCePhase2.2MISC"), stringsAsFactors = FALSE)
-  
-  meds_of_interest <- read.delim(system.file(paste0("extdata", .Platform$file.sep,
-                                                    "medicationCharacteristics.txt"), package = "FourCePhase2.2MISC"), stringsAsFactors = FALSE)
-  
-  procs_of_interest <- c()
-  diag_of_interest <- c()
-  
-  ### create laboratory value summary
-  # include: max, min, and mean values
-  # include: number of patients and %
-  # calculate neutrophil ratio?
-  
-  lab_sum <- complete_df %>%
-    filter(concept_code %in% labs_of_interest$concept_code) %>%
-    select(siteid, cohort, patient_num, concept_code, value) %>%
-    left_join(labs_of_interest, by = 'concept_code') %>%
-    filter( value != -999,
-            ! is.na( value ))
-  print(paste0("There are ", n_distinct(lab_sum$concept_code), " lab codes of interest reported in the data"))
-  
-  
-  # lab summary table
-  if(n_distinct(lab_sum$concept_code) != 0 ){
-    lab_sum <- lab_sum %>%
-      group_by(variableName) %>%
-      summarise(units = first(units),
-                min_value = min(value, na.rm = TRUE),
-                max_value = max(value, na.rm = TRUE),
-                mean_value = mean(value, na.rm = TRUE),
-                sd_value = sd(value, na.rm = TRUE),
-                n_patients = n_distinct(patient_num)) %>%
-      mutate( n_patients = ifelse( n_patients > obfuscation_threshold | isFALSE( obfuscation_threshold), n_patients, 0.5),
-              perc_patients = (n_patients / total_n) * 100,
-              siteid = currSiteId)
-    
-    #write.table(lab_sum, paste0(dir.output,'/QC/', currSiteId, '_MISC', 'lab_summary.txt'), quote = FALSE, row.names = FALSE)
-    print(lab_sum)
-  }
-  
-  
-  ### create medication value summary
-  # include: number of patients and %
-  meds <- complete_df %>%
-    filter(concept_type == 'MED-CLASS') %>%
-    pull(concept_code) %>%
-    unique()
-  print("These are the meds that are present for MISC patients: ")
-  print(meds)
-  
-  med_sum <- complete_df %>%
-    filter(concept_code %in% meds_of_interest$concept_code) %>%
-    select(siteid, cohort, patient_num, concept_code, value) %>%
-    left_join(meds_of_interest, by = 'concept_code')
-  print(paste0("There are ", n_distinct(med_sum$concept_code), " medication codes of interest reported in the data"))
-  
-  if( n_distinct(med_sum$concept_code) != 0){
-    # medication summary table
-    med_sum <- med_sum %>%
-      group_by(variableName) %>%
-      summarise( n_patients = n_distinct(patient_num)) %>%
-      mutate(n_patients = ifelse( n_patients > obfuscation_threshold | isFALSE( obfuscation_threshold), n_patients, 0.5),
-             perc_patients = (n_patients / total_n) * 100,
-             siteid = currSiteId)
-    
-    #write.table(med_sum, paste0(dir.output, '/QC/', currSiteId, '_MISC', 'medication_summary.txt'), quote = FALSE, row.names = FALSE)
-    print(med_sum)
-  }
-  
-  if( 'PROC-GROUP' %in% complete_df$concept_type){
-    ### create procedures value summary
-    # include: number of patients and %
-    proc_sum <- complete_df %>%
-      filter(concept_type == 'PROC-GROUP') %>%
-      group_by(concept_code) %>%
-      summarise(n_patients = n_distinct(patient_num)) %>%
-      mutate( n_patients = ifelse( n_patients > obfuscation_threshold | isFALSE( obfuscation_threshold), n_patients, 0.5),
-              perc_patients = (n_patients / total_n) * 100,
-              siteid = currSiteId)
-    print(paste0("There are ", n_distinct(proc_sum$concept_code), " procedural codes reported in the data"))
-    
-    
-    print(proc_sum)
-    
-    #write.table(proc_sum, paste0(dir.output, '/QC/', currSiteId, '_MISC', 'procedure_summary.txt'), quote = FALSE, row.names = FALSE)
-  }
-  
-  if( 'DIAG-ICD10' %in% complete_df$concept_type){
-    # add a summary of the ICD codes for QC
-    diag_sum <- complete_df %>%
-      filter( concept_type == 'DIAG-ICD10') %>%
-      group_by( concept_code ) %>%
-      summarise( n_patients = n_distinct( patient_num ) ) %>%
-      mutate( n_patients = ifelse( n_patients > obfuscation_threshold | isFALSE( obfuscation_threshold), n_patients, 0.5)) %>%
-      arrange( desc(n_patients))
-    
-    save( diag_sum, file=paste0( dir.output, "/QC/", currSiteId, "_ICDdiagnosisCodes.RData"))
-    
-    print(diag_sum[1:10,])
-  }
-  
+  ### total patients should be the same that total number of patients ever hospitalized
+  ### QC is done after filtering by hospitalized patients 
+  total_n <- length(unique(input_df$patient_num))
+  print(paste0("Total patients : ", total_n))
+
 }
