533dc872a3c2e4dd33a3fe0cb1f07191ee606076
aGutierrezSacristan <simranmakw@gmail.com>
2023-05-24 18:02:41 +0000

    End date updated

--- R/setUpVariableParameters.R
+++ R/setUpVariableParameters.R
@@ -11,7 +11,7 @@ time_period <-"months"
 ## cut-off dates
 pandemic_start_date <- as.Date("2020-03-01")
 start_date_plots <- as.Date("2018-12-01")
-end_date_plots <- as.Date("2023-01-03")
+end_date_plots <- as.Date("2023-03-03")
 
 ## Colorblind palette
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
