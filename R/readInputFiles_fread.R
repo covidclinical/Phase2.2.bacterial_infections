6022b1f9cc65f26cab696935bf762a14a8b9d0b4
Simran Makwana <simranmakw@gmail.com>
2023-06-01 14:14:02 +0000

    Update fread to not duplicate data

--- R/readInputFiles_fread.R
+++ R/readInputFiles_fread.R
@@ -1,4 +1,4 @@
-readInputFilesFread <- function( path, separator = ",", skip = 0, verbose = FALSE, ... ){
+checkInputFilesFread <- function( path, separator = ",", skip = 0, verbose = FALSE, ... ){
 
   #check that the input files needed are in the path
   if( verbose == TRUE){
@@ -14,43 +14,24 @@ readInputFilesFread <- function( path, separator = ",", skip = 0, verbose = FALS
   check_files <- fourcefile_names %in% filesInDirectory
   if (all(check_files)) {
     if (verbose) {
-      cat("All of ", fourcefile_names, " are in the directory")
+      cat("All of ", fourcefile_names, " are in the directory, input file check passed")
     }
+    return(TRUE)
   } else {
     cat("Following files not found in the file directory:\n")
     cat(fourcefile_names[!check_files])
     cat("\n")
     cat("Please check if the file name and the directory are correct")
-    stop()
+    return(FALSE)
   }
 
-  #read the files
-  if( verbose == TRUE){
-    cat( 'Reading \n')
-    cat(fourcefile_names)
-    cat(" files")
-  }
+}
 
-  read_delim_4ce <- function(file_name, ...) data.table::fread(file.path(path, file_name), sep = separator, skip = skip, ...)
-  patientSummary <- read_delim_4ce("LocalPatientSummary.csv")
-  patientObservations <- read_delim_4ce("LocalPatientObservations.csv")
-  patientClinicalCourse <- read_delim_4ce("LocalPatientClinicalCourse.csv")
+  
+read_delim_4ce <- function(file_name, path, separator = ",", skip = 0, verbose = FALSE) {
+  print(paste0('reading: ', file_name))
+  df <- data.table::fread(file.path(path, file_name), sep = separator, skip = skip)
+  print( paste0( file_name, " contains: ", nrow( df ), " rows and ", ncol( df ), " columns."))
+  return(df)
 
-  if( verbose == TRUE){
-    print( paste0( "LocalPatientsummary file contains: ", nrow( patientSummary ), " rows and ", ncol( patientSummary ), " columns."))
-    print( paste0( "LocalPatientobservation file contains: ", nrow( patientObservations ), " rows and ", ncol( patientObservations ), " columns."))
-    print( paste0( "LocalPatientClinicalCourse file contains: ", nrow( patientClinicalCourse ), " rows and ", ncol( patientClinicalCourse ), " columns."))
-  }
-
-  #return it as a list
-  files <- list("patientSummary"        = patientSummary,
-                "patientObservations"   = patientObservations,
-                "patientClinicalCourse" = patientClinicalCourse
-  )
-
-  if( verbose == TRUE){
-    print( "A list with the three data.frames read is being generated")
-  }
-
-  return( files )
 }
