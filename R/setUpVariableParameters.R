a76e419f9cf4a73c6e0f167beb54db286b73b36f
aGutierrezSacristan <simranmakw@gmail.com>
2023-06-01 13:53:40 +0000

    Update dates

--- R/setUpVariableParameters.R
+++ R/setUpVariableParameters.R
@@ -11,11 +11,11 @@ time_period <-"months"
 ## cut-off dates
 pre_NPI <- as.Date( "2020-02-01") 
 full_NPI <- as.Date( "2021-02-01")
-partial_NPI <- as.Date("2023-02-01")
+partial_NPI <- as.Date("2022-07-01")
 
 ## plots start and end dates
 start_date_plots <- as.Date("2018-12-01")
-end_date_plots <- as.Date("2023-03-03")
+end_date_plots <- as.Date("2022-08-01")
 
 ## Colorblind palette
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
