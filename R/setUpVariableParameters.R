f8c65e1834e8a46e04fd80c1441949882f07322d
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2023-06-26 12:41:18 +0000

    change the end date to 2023

--- R/setUpVariableParameters.R
+++ R/setUpVariableParameters.R
@@ -11,11 +11,11 @@ time_period <-"month"
 ## cut-off dates
 pre_NPI <- as.Date( "2020-02-01") 
 full_NPI <- as.Date( "2021-02-01")
-partial_NPI <- as.Date("2022-07-01")
+partial_NPI <- as.Date("2023-03-01")
 
 ## plots start and end dates
 start_date_plots <- as.Date("2018-12-01")
-end_date_plots <- as.Date("2022-08-01")
+end_date_plots <- as.Date("2023-04-01")
 
 ## Colorblind palette
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
